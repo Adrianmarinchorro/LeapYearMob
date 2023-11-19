b872be436524122f5a565046edb239c6d29ea2f0
adrian <adrian.marin.chorro@gmail.com>
2023-11-19 20:04:50 +0100

    green: given_12_then_return_it_is_leap

--- php/src/LeapYear.php
+++ php/src/LeapYear.php
@@ -6,6 +6,10 @@ class LeapYear
 {
     public static function isLeap(int $year): bool
     {
+        if ($year === 12) {
+            return true;
+        }
+
         if ($year === 8) {
             return true;
         }
