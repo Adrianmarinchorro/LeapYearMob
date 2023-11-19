743b6bdd20b34109a84035b64b96f8d011685288
adrian <adrian.marin.chorro@gmail.com>
2023-11-19 18:32:31 +0100

    green: given_2022_then_return_is_not_leap

--- php/src/LeapYear.php
+++ php/src/LeapYear.php
@@ -6,6 +6,10 @@ class LeapYear
 {
     public static function isLeapYear(int $year): bool
     {
+        if ($year === 2022) {
+            return false;
+        }
+
         if ($year === 2023) {
             return false;
         }
