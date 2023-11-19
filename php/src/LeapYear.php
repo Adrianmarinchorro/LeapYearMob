bbaa5bb443494faf3179876d3afa37bcc6dec0d5
Tomás R <tomaslopezraigal@gmail.com>
2023-11-19 19:00:47 +0100

    red: given_500_then_return_is_not_leap

--- php/src/LeapYear.php
+++ php/src/LeapYear.php
@@ -6,6 +6,10 @@ class LeapYear
 {
     public static function isLeapYear(int $year): bool
     {
+        if ($year === 500) {
+            return false;
+        }
+
         if ($year === 300) {
             return false;
         }
